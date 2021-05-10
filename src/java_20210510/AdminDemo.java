d0314de08ac39712a228d4c03bda7733ff8c8ced
jeongke <jeongke26@naver.com>
2021-05-10 16:25:02 +0900

    2021-05-10-v2

---
+++ src/java_20210510/AdminDemo.java
@@ -0,0 +1,16 @@
+package java_20210510;
+
+public class AdminDemo {
+	public static void main(String[] args) {
+		Admin a = new Admin("jeongke26","123","jeongke26@naver.com",20);
+		
+		System.out.println(a.getId());
+		System.out.println(a.getPwd());
+		System.out.println(a.getEmail());
+		System.out.println(a.getLevel());
+		
+
+		
+		
+	}
+}
---
+++ src/java_20210510/aa/AdminDemo.java
@@ -0,0 +1,12 @@
+package java_20210510.aa;
+import java_20210510.Admin;
+public class AdminDemo {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Admin a =new Admin();
+		
+	}
+
+}
